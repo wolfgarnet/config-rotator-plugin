389c8b30163974618d0f99298ddf211dc205d289
wolfgarnet <wolfgarnet@gmail.com>
2012-04-11 16:22:23 +0200

    Added first unit test

---
+++ src/test/java/net/praqma/configrotator/ConfigTest.java
@@ -0,0 +1,63 @@
+package net.praqma.configrotator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import hudson.model.Cause;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+
+import org.junit.Test;
+
+import net.praqma.jenkins.configrotator.ConfigurationRotator;
+import net.praqma.jenkins.configrotator.ConfigurationRotatorBuildAction;
+import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseUCM;
+import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseUCMTarget;
+import net.praqma.jenkins.plugin.drmemory.DrMemoryBuildAction;
+import net.praqma.jenkins.utils.test.ClearCaseJenkinsTestCase;
+
+public class ConfigTest extends ClearCaseJenkinsTestCase {
+
+	@Test
+	public void test1() throws Exception {
+		coolTest.bootStrap();
+		
+		FreeStyleProject project = createFreeStyleProject( "configrotator" );
+		
+		ClearCaseUCM ccucm = new ClearCaseUCM( coolTest.getPVob().toString() );
+		List<ClearCaseUCMTarget> targets = new ArrayList<ClearCaseUCMTarget>();
+		targets.add( new ClearCaseUCMTarget( "_System_1.0@" + coolTest.getPVob() + ", INITIAL, false" ) );
+		ccucm.targets = targets;
+		
+		ConfigurationRotator cr = new ConfigurationRotator( ccucm, true );
+		
+		project.setScm( cr );
+		
+		
+		FreeStyleBuild b = project.scheduleBuild2( 0 ).get();
+		
+		System.out.println( "Workspace: " + b.getWorkspace() );
+		
+		ConfigurationRotatorBuildAction action = b.getAction( ConfigurationRotatorBuildAction.class );
+		
+		System.out.println( "Action: " + action );
+		System.out.println( "Logfile: " + b.getLogFile() );
+		
+		BufferedReader br = new BufferedReader( new FileReader( b.getLogFile() ) );
+		String line = "";
+		while( ( line = br.readLine() ) != null ) {
+			System.out.println( "[JENKINS] " + line );
+		}
+		
+		if( action != null ) {
+			System.out.println( "Action: " + action.getResult() );
+		} else {
+			System.out.println( "ACTION IS NULL" );
+		}
+		
+		assertNotNull( action );
+	}
+}
