0bf440792e740d171816a3627d64fa6db419c8c8
Bue Petersen <bue@mylaptop.(none)>
2012-04-13 12:55:33 +0200

    One more test to try out teststuff

--- src/test/java/net/praqma/jenkins/configrotator/ConfigTest.java
+++ src/test/java/net/praqma/jenkins/configrotator/ConfigTest.java
@@ -2,19 +2,16 @@ package net.praqma.jenkins.configrotator;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import hudson.model.Cause;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 
 import org.junit.Test;
 
-import net.praqma.jenkins.configrotator.ConfigurationRotator;
-import net.praqma.jenkins.configrotator.ConfigurationRotatorBuildAction;
 import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseUCM;
+import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseUCMConfiguration;
 import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseUCMTarget;
 import net.praqma.jenkins.utils.test.ClearCaseJenkinsTestCase;
 
@@ -64,5 +61,55 @@ public class ConfigTest extends ClearCaseJenkinsTestCase {
 		}
 		
 		assertNotNull( action );
+	}
+  
+  
+  @Test
+	public void manualIterateThroughAllBaselines() throws Exception {
+		
+		System.out.println( "Started test 'manualIterateThroughAllBaselines'" );
+		
+		coolTest.bootStrap();
+		System.out.println( "coolTest.bootStrap done." );
+		
+		FreeStyleProject project = createFreeStyleProject( "configrotator" );
+		
+		/*  */
+		ClearCaseUCM ccucm = new ClearCaseUCM( coolTest.getPVob().toString() );
+		List<ClearCaseUCMTarget> targets = new ArrayList<ClearCaseUCMTarget>();
+		targets.add( new ClearCaseUCMTarget( "model-1@" + coolTest.getPVob() + ", INITIAL, false" ) ); // prøv forkert navn
+    targets.add( new ClearCaseUCMTarget( "client-1@" + coolTest.getPVob() + ", INITIAL, false" ) );
+		ccucm.targets = targets;
+		
+		ConfigurationRotator cr = new ConfigurationRotator( ccucm, true );
+		project.setScm( cr );
+		
+    // scheduling a build should get new baseline for model: model-2
+		FreeStyleBuild b = project.scheduleBuild2( 0 ).get();
+		
+    System.out.println( "Workspace: " + b.getWorkspace() );
+		ConfigurationRotatorBuildAction action = b.getAction( ConfigurationRotatorBuildAction.class );
+		System.out.println( "Action: " + action );
+		System.out.println( "Logfile: " + b.getLogFile() );
+    
+    BufferedReader br = new BufferedReader( new FileReader( b.getLogFile() ) );
+		String line = "";
+		while( ( line = br.readLine() ) != null ) {
+			System.out.println( "[JENKINS] " + line );
+		}
+		
+    // this test plan to iterate one baseline at a time
+    // ... for now, just printing stuff out to se what I get
+		if( action != null ) {
+			System.out.println( "Action: " + action.getResult() );
+      ClearCaseUCMConfiguration test = (ClearCaseUCMConfiguration) action.getConfiguration();
+      System.out.println( "getShortname()" + test.getList().get(0).getBaseline().getShortname());
+      System.out.println( "getComment()" + test.getList().get(0).getBaseline().getComment());
+      System.out.println( "getPVob()" + test.getList().get(0).getBaseline().getPVob());
+		} else {
+			System.out.println( "ACTION IS NULL" );
+		}
+		assertNotNull( action );
+    
 	}
 }
