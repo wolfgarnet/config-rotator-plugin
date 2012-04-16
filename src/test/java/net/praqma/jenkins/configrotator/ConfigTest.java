11fddfe83ce7d5b5a3f215e7e49cdf17a3cbe254
Bue Petersen <bue@praqma.net>
2012-04-16 15:02:05 +0200

    Tests stuff, commented out more of test1

--- src/test/java/net/praqma/jenkins/configrotator/ConfigTest.java
+++ src/test/java/net/praqma/jenkins/configrotator/ConfigTest.java
@@ -17,60 +17,60 @@ import net.praqma.jenkins.utils.test.ClearCaseJenkinsTestCase;
 
 public class ConfigTest extends ClearCaseJenkinsTestCase {
 
-	//@Test
-	public void test1() throws Exception {
-		
-		System.out.println( "I AMMMMM HEHREHHREHEHRHEHHERHHERHE" );
-		
-    String uniqueTestVobName = System.getenv("BUILD_TAG");
-    if (uniqueTestVobName.isEmpty())
-    {// NOT running under jenkins!
-      uniqueTestVobName = "config-rotator-plugin-test1" + (System.currentTimeMillis()/1000);
-    }
-    // else using name telling it is a Jenkins job
-    
-    coolTest.variables.put("vobname", uniqueTestVobName );
-    coolTest.variables.put("pvobname", uniqueTestVobName );
-		coolTest.bootStrap();
-    		
-		System.out.println( "AFTER" );
-		
-		FreeStyleProject project = createFreeStyleProject( "configrotator" );
-		
-		/*  */
-		ClearCaseUCM ccucm = new ClearCaseUCM( coolTest.getPVob().toString() );
-		List<ClearCaseUCMTarget> targets = new ArrayList<ClearCaseUCMTarget>();
-		targets.add( new ClearCaseUCMTarget( "model-1@" + coolTest.getPVob() + ", INITIAL, false" ) );
-		ccucm.targets = targets;
-		
-		ConfigurationRotator cr = new ConfigurationRotator( ccucm, true );
-		
-		project.setScm( cr );
-		
-		
-		FreeStyleBuild b = project.scheduleBuild2( 0 ).get();
-		
-		System.out.println( "Workspace: " + b.getWorkspace() );
-		
-		ConfigurationRotatorBuildAction action = b.getAction( ConfigurationRotatorBuildAction.class );
-		
-		System.out.println( "Action: " + action );
-		System.out.println( "Logfile: " + b.getLogFile() );
-		
-		BufferedReader br = new BufferedReader( new FileReader( b.getLogFile() ) );
-		String line = "";
-		while( ( line = br.readLine() ) != null ) {
-			System.out.println( "[JENKINS] " + line );
-		}
-		
-		if( action != null ) {
-			System.out.println( "Action: " + action.getResult() );
-		} else {
-			System.out.println( "ACTION IS NULL" );
-		}
-		
-		assertNotNull( action );
-	}
+//  @Test
+//	public void test1() throws Exception {
+//		
+//		System.out.println( "I AMMMMM HEHREHHREHEHRHEHHERHHERHE" );
+//		
+//    String uniqueTestVobName = System.getenv("BUILD_TAG");
+//    if (uniqueTestVobName.isEmpty())
+//    {// NOT running under jenkins!
+//      uniqueTestVobName = "config-rotator-plugin-test1" + (System.currentTimeMillis()/1000);
+//    }
+//    // else using name telling it is a Jenkins job
+//    
+//    coolTest.variables.put("vobname", uniqueTestVobName );
+//    coolTest.variables.put("pvobname", uniqueTestVobName );
+//		coolTest.bootStrap();
+//    		
+//		System.out.println( "AFTER" );
+//		
+//		FreeStyleProject project = createFreeStyleProject( "configrotator" );
+//		
+//		/*  */
+//		ClearCaseUCM ccucm = new ClearCaseUCM( coolTest.getPVob().toString() );
+//		List<ClearCaseUCMTarget> targets = new ArrayList<ClearCaseUCMTarget>();
+//		targets.add( new ClearCaseUCMTarget( "model-1@" + coolTest.getPVob() + ", INITIAL, false" ) );
+//		ccucm.targets = targets;
+//		
+//		ConfigurationRotator cr = new ConfigurationRotator( ccucm, true );
+//		
+//		project.setScm( cr );
+//		
+//		
+//		FreeStyleBuild b = project.scheduleBuild2( 0 ).get();
+//		
+//		System.out.println( "Workspace: " + b.getWorkspace() );
+//		
+//		ConfigurationRotatorBuildAction action = b.getAction( ConfigurationRotatorBuildAction.class );
+//		
+//		System.out.println( "Action: " + action );
+//		System.out.println( "Logfile: " + b.getLogFile() );
+//		
+//		BufferedReader br = new BufferedReader( new FileReader( b.getLogFile() ) );
+//		String line = "";
+//		while( ( line = br.readLine() ) != null ) {
+//			System.out.println( "[JENKINS] " + line );
+//		}
+//		
+//		if( action != null ) {
+//			System.out.println( "Action: " + action.getResult() );
+//		} else {
+//			System.out.println( "ACTION IS NULL" );
+//		}
+//		
+//		assertNotNull( action );
+//	}
   
   
   @Test
