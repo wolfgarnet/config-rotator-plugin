65ff5dbb4c8edc34273efdef7759ee96aa671ef6
Mads Nielsen <inkspot84@gmail.com>
2012-04-17 11:04:22 +0200

    Polling experiment

--- src/test/java/net/praqma/jenkins/configrotator/ConfigTest.java
+++ src/test/java/net/praqma/jenkins/configrotator/ConfigTest.java
@@ -1,12 +1,14 @@
 package net.praqma.jenkins.configrotator;
 
+import hudson.console.ConsoleNote;
+import hudson.model.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import hudson.model.FreeStyleBuild;
-import hudson.model.FreeStyleProject;
+import hudson.scm.PollingResult;
+import java.io.*;
 
 import org.junit.Test;
 
@@ -174,4 +176,81 @@ public class ConfigTest extends ClearCaseJenkinsTestCase {
         }
         while ((t1 - t0) < (seconds * 1000));
     }
+	
+	@Test
+	public void testPollSCMChange() throws Exception {
+    String testName = "PollSCMChange";
+    String debugLine = "'" + testName + "': ";
+    System.out.println( debugLine + "Starting" );
+    // ONLY alphanumeric chars
+		String uniqueTestVobName = testName + uniqueTimeStamp;
+    
+    // set up cool to run tests with ClearCase environment
+    // variables overwrite cool test case setup.xml setting
+    // Unique names for each test is used to avoid all sort of clear case 
+    // complications - but leaves as mess...
+    coolTest.variables.put("vobname", uniqueTestVobName );
+    coolTest.variables.put("pvobname", uniqueTestVobName );
+		coolTest.bootStrap();
+		System.out.println( debugLine + "Setup done." );
+		
+    // create Jenkins job - also use unique name
+		FreeStyleProject project = createFreeStyleProject( uniqueTestVobName );
+		// Setup ClearCase UCM as SCM and to use with config-rotator
+		ClearCaseUCM ccucm = new ClearCaseUCM( coolTest.getPVob().toString() );
+		List<ClearCaseUCMTarget> targets = new ArrayList<ClearCaseUCMTarget>();
+		targets.add( new ClearCaseUCMTarget( "model-1@" + coolTest.getPVob() + ", INITIAL, false" ) ); // prøv forkert navn
+    targets.add( new ClearCaseUCMTarget( "client-1@" + coolTest.getPVob() + ", INITIAL, false" ) );
+		ccucm.targets = targets;
+    // create config-rotator, and set it as SCM
+		ConfigurationRotator cr = new ConfigurationRotator( ccucm, true );
+		project.setScm( cr );
+	
+		
+		ConfigurationRotatorBuildAction action = null; 
+		Run build = null;
+		//Project setup done. Now attempt to poll for changes.
+		System.out.println(debugLine + "Polling project.");
+		project.schedulePolling();
+		
+		build = project.getLastBuild();
+		
+		if(build != null) {
+			action = build.getAction(ConfigurationRotatorBuildAction.class);
+			System.out.println(debugLine+"Last build result was: "+action.getResult());
+		} else {
+			System.out.println(debugLine+"No previous build, this is the first build.");
+		}
+		
+		System.out.println( "Action: " + action );
+		System.out.println( "Logfile: " + build.getLogFile() );
+		
+		BufferedReader br = new BufferedReader( new FileReader( build.getLogFile() ) );
+		String line = "";
+		while( ( line = br.readLine() ) != null ) {
+			System.out.println( "[JENKINS] " + line );
+		}
+		
+    // this test plan to iterate one baseline at a time
+    // ... for now, just printing stuff out to se what I get
+		if( action != null ) {
+			System.out.println( "Action: " + action.getResult() );
+      ClearCaseUCMConfiguration test = (ClearCaseUCMConfiguration) action.getConfiguration();
+      System.out.println( "getShortname()" + test.getList().get(0).getBaseline().getShortname());
+      System.out.println( "getComment()" + test.getList().get(0).getBaseline().getComment());
+      System.out.println( "getPVob()" + test.getList().get(0).getBaseline().getPVob());
+		} else {
+			System.out.println( "ACTION IS NULL" );
+		}
+    // NOTICE - this is very IMPORTANT to avoid Jenkins error on cleaning 
+    // temporary dirs after jobs completes meaning test fails
+    br.close();
+		
+    // waiting is important to ensure unique timestamps and let Jenkins clean
+    // workspace after each test
+    waiting(watingSeconds);
+        
+    // Build action should not be null
+		assertNotNull( action );
+	}
 }
