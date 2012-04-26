ca78d0bea78745ad4611d5805669f1dbd19b0aaf
Mads Nielsen <inkspot84@gmail.com>
2012-04-26 18:19:41 +0200

    Refactored feed page, added static factory method for unique feed urls

--- src/main/java/net/praqma/jenkins/configrotator/ConfigurationRotatorReport.java
+++ src/main/java/net/praqma/jenkins/configrotator/ConfigurationRotatorReport.java
@@ -16,6 +16,8 @@ import hudson.model.AbstractModelObject;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import javax.security.auth.login.Configuration;
 import jenkins.model.Jenkins;
 
 // FIXME - RootAction - needs login ? Should it be public?
@@ -44,15 +46,27 @@ public class ConfigurationRotatorReport extends AbstractModelObject implements U
 		return getUrlName();
 	}
     
-    public String getFeedUrl(String componentXmlFile) {
-        String actionLink = "/"+getUrlName()+"/feed/?component="+getComponentName(componentXmlFile);
+    /**
+     * Factory to create url when subscribing to feeds
+     * 
+     */ 
+    public static String CreateFeedUrl(String vobname, String componentName) {
+        String actionLink = "/"+ConfigurationRotator.NAME+"/feed/?component="+componentName+"&pvob="+vobname;
+        return actionLink;
+    }
+    
+    public String getFeedUrl(String vob, String componentName) {
+        String actionLink = "/"+getUrlName()+"/feed/?component="+componentName+"&pvob="+vob;
         return actionLink;
     }
     
     public String getComponentName(String componentXmlFile) {
         return componentXmlFile.substring(0,componentXmlFile.indexOf("."));
-    } 
-            
+    }
+    
+    public String getVobName(File f) {
+        return f.getName();
+    }
     
     /**
      * 
@@ -64,13 +78,53 @@ public class ConfigurationRotatorReport extends AbstractModelObject implements U
         return list;
     }
     
+    public ArrayList<String> listAvailableFeeds(List<String> vobs) {
+        ArrayList<String> list = new ArrayList<String>();
+        for(String s: vobs) {
+            
+        }
+        
+        return list;
+    }
+    
+    /**
+     * Extracts vobs. Use this as an argument to listAvailableFeeds
+     * 
+     */ 
+    public ArrayList<File> listVobs() {
+        FileFilter listDirsFilter = new FileFilter() {
+
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isDirectory();
+            }
+        };
+        
+        ArrayList<File> list = new ArrayList<File>();
+        
+        for(File f: ConfigurationRotator.FEED_DIRFILE.listFiles(listDirsFilter)) {
+            list.add(f);
+        }
+        
+        return list;
+    }
+    
+    public ArrayList<String> listComponents(File f) {
+        ArrayList<String> components = new ArrayList<String>();
+        components.addAll(Arrays.asList(f.list()));
+        return components;
+    }
+    
+    
+    
+    
     
 
 	// yourhost/config-rotator/feed/
 	// eg. http://localhost:8080/config-rotator/feed/?component=hest2
     // TODO: input parameter check and validation
-	public HttpResponse doFeed( @QueryParameter( required = true ) String component) throws ServletException, IOException {
-		final String mycomp = component+XML_EXTENSION;
+	public HttpResponse doFeed( @QueryParameter( required = true ) String component, @QueryParameter( required = true ) String pvob) throws ServletException, IOException {
+		final String mycomp = pvob+ConfigurationRotator.SEPARATOR+component+XML_EXTENSION;
         final String fullComponentFeedPath = ConfigurationRotator.FEED_FULL_PATH+mycomp;
 		return new HttpResponse() {
 
