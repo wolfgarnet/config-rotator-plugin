ef64845237a519178d9a60a4bb21f136aa10f183
wolfgarnet <wolfgarnet@gmail.com>
2012-05-03 10:11:50 +0200

    Changed toString to handle nulls

--- src/main/java/net/praqma/jenkins/configrotator/ConfigurationRotatorBuildAction.java
+++ src/main/java/net/praqma/jenkins/configrotator/ConfigurationRotatorBuildAction.java
@@ -89,6 +89,6 @@ public class ConfigurationRotatorBuildAction implements Action {
     
 	@Override
 	public String toString() {
-		return "BUILD ACTION: " + configuration.toString();
+		return "Build action: " + configuration;
 	}
 }
