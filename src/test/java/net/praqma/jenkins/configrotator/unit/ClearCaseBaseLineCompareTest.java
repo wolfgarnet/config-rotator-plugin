b581fd41c9e4f57c14183332a6ddef3bcf446ea1
Mads Nielsen <inkspot84@gmail.com>
2012-05-08 18:57:54 +0200

    Started work on ClearCaseBaseLineCompare test

---
+++ src/test/java/net/praqma/jenkins/configrotator/unit/ClearCaseBaseLineCompareTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.praqma.jenkins.configrotator.unit;
+
+import hudson.model.BuildListener;
+import hudson.remoting.VirtualChannel;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.TestCase;
+import net.praqma.jenkins.configrotator.AbstractConfiguration;
+import net.praqma.jenkins.configrotator.ConfigurationRotatorException;
+import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseActivity;
+import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseGetBaseLineCompare;
+import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseUCMConfiguration;
+import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseUCMConfigurationComponent;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+
+/**
+ *
+ * @author Praqma
+ */
+public class ClearCaseBaseLineCompareTest extends TestCase {
+    
+    BuildListener buildlistener;
+    ClearCaseGetBaseLineCompare compare;
+    VirtualChannel channel;
+    ClearCaseUCMConfiguration confone;
+    ClearCaseUCMConfiguration conftwo = new ClearCaseUCMConfiguration();
+    
+    
+    @Before
+	public void initialize() throws IOException, InterruptedException, ConfigurationRotatorException {
+        channel = PowerMockito.mock(VirtualChannel.class);
+        confone = PowerMockito.mock(ClearCaseUCMConfiguration.class);
+        buildlistener = PowerMockito.mock(BuildListener.class);
+        compare = new ClearCaseGetBaseLineCompare(buildlistener, confone, conftwo);
+        PowerMockito.when(confone.difference(Mockito.any(ClearCaseUCMConfiguration.class))).thenReturn(new ArrayList<ClearCaseActivity>());
+
+    }
+    
+    
+    /**
+     * TODO:IMPLEMENT ME
+     */
+    @Test
+    public void testClearCaseBaseLineCompare() {
+        try {
+            assertTrue(true);
+        } catch (Exception ex) {
+            System.out.println(ex);
+            fail();
+        }
+    }
+}
