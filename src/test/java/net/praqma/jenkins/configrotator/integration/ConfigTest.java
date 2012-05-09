573c6259287bc118888e5a964e8f2ca9db1de8be
Bue Petersen <bue@praqma.net>
2012-05-09 13:18:22 +0200

    Trying cooltest.teardown in one test to get more error message

--- src/test/java/net/praqma/jenkins/configrotator/integration/ConfigTest.java
+++ src/test/java/net/praqma/jenkins/configrotator/integration/ConfigTest.java
@@ -91,10 +91,10 @@ public class ConfigTest extends ClearCaseJenkinsTestCase {
 
         
         // FIXME - does not work every time either.
-//        System.out.println(debugLine + "Trying teardown with net.praqma.clearcase.util.SetupUtils.tearDown(coolTest.getPVob())");
-//        coolTest.tearDown();
-//        net.praqma.clearcase.util.SetupUtils.tearDown(coolTest.getPVob());
-//        System.out.println(debugLine + "Done calling teardown....");
+        System.out.println(debugLine + "Trying teardown with net.praqma.clearcase.util.SetupUtils.tearDown(coolTest.getPVob())");
+        coolTest.tearDown();
+        net.praqma.clearcase.util.SetupUtils.tearDown(coolTest.getPVob());
+        System.out.println(debugLine + "Done calling teardown....");
 
 
         System.out.println(debugLine + "Test done - waiting... trying avoid Jenkins failing due to clean temp dirs error");
