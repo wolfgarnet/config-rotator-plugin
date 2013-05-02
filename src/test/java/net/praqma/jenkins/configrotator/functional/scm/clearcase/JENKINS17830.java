916ef57f6e9de0b2f47a8259230a9ad7edb9cf75
Christian Wolfgang <wolfgarnet@gmail.com>
2013-05-02 14:18:55 +0200

    [JENKINS-17830] Adding test

---
+++ src/test/java/net/praqma/jenkins/configrotator/functional/scm/clearcase/JENKINS17830.java
@@ -0,0 +1,66 @@
+package net.praqma.jenkins.configrotator.functional.scm.clearcase;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Result;
+import net.praqma.clearcase.test.junit.ClearCaseRule;
+import net.praqma.jenkins.configrotator.ConfigRotatorProject;
+import net.praqma.jenkins.configrotator.ConfigRotatorRule2;
+import net.praqma.jenkins.configrotator.ProjectBuilder;
+import net.praqma.jenkins.configrotator.SystemValidator;
+import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseUCM;
+import net.praqma.jenkins.configrotator.scm.clearcaseucm.ClearCaseUCMTarget;
+import net.praqma.logging.PraqmaticLogFormatter;
+import net.praqma.util.test.junit.LoggingRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * @author cwolfgang
+ */
+public class JENKINS17830 {
+
+    private static Logger logger = Logger.getLogger( JENKINS17830.class.getName() );
+
+    public static ClearCaseRule ccenv = new ClearCaseRule( "JENKINS17830", "setup-rootless.xml" );
+
+    public static LoggingRule lrule = new LoggingRule( "net.praqma" ).setFormat( PraqmaticLogFormatter.TINY_FORMAT );
+
+    @ClassRule
+    public static TestRule chain = RuleChain.outerRule( lrule ).around( ccenv );
+
+    @ClassRule
+    public static ConfigRotatorRule2 crrule = new ConfigRotatorRule2( JENKINS17830.class );
+
+    @Test
+    public void test() throws IOException {
+        ProjectBuilder builder = new ProjectBuilder( new ClearCaseUCM( ccenv.getPVob() ) ).setName( "remove-target" );
+        ConfigRotatorProject project = builder.getProject();
+        project.addTarget( new ClearCaseUCMTarget( "_Server@" + ccenv.getPVob() + ", INITIAL, false" ) ).
+                addTarget( new ClearCaseUCMTarget( "_Client@" + ccenv.getPVob() + ", INITIAL, false" ) );
+
+        AbstractBuild<?, ?> build = crrule.buildProject( project.getJenkinsProject(), false, null );
+
+        SystemValidator<ClearCaseUCMTarget> val = new SystemValidator<ClearCaseUCMTarget>( build );
+        val.checkExpectedResult( Result.SUCCESS ).
+                checkAction( true ).
+                checkTargets( new ClearCaseUCMTarget( "server-1@" + ccenv.getPVob() + ", INITIAL, false" ), new ClearCaseUCMTarget( "client-1@" + ccenv.getPVob() + ", INITIAL, false" ) ).
+                checkCompatability( true ).
+                validate();
+
+        project.reconfigure().addTarget( new ClearCaseUCMTarget( "server-1@" + ccenv.getPVob() + ", INITIAL, false" ) );
+
+        AbstractBuild<?, ?> build2 = crrule.buildProject( project.getJenkinsProject(), false, null );
+
+        SystemValidator<ClearCaseUCMTarget> val2 = new SystemValidator<ClearCaseUCMTarget>( build2 );
+        val2.checkExpectedResult( Result.SUCCESS ).
+                checkAction( true ).
+                checkTargets( new ClearCaseUCMTarget( "server-1@" + ccenv.getPVob() + ", INITIAL, false" ) ).
+                checkCompatability( true ).
+                validate();
+    }
+}
