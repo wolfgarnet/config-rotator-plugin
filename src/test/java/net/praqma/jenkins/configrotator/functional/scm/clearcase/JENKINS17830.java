cfc873d57c54c4c56b975868b8b186f76e70c12a
Christian Wolfgang <wolfgarnet@gmail.com>
2013-05-07 15:03:23 +0200

    [JENKINS-17830] Updated test

--- src/test/java/net/praqma/jenkins/configrotator/functional/scm/clearcase/JENKINS17830.java
+++ src/test/java/net/praqma/jenkins/configrotator/functional/scm/clearcase/JENKINS17830.java
@@ -1,6 +1,8 @@
 package net.praqma.jenkins.configrotator.functional.scm.clearcase;
 
+import hudson.FilePath;
 import hudson.model.AbstractBuild;
+import hudson.model.FreeStyleProject;
 import hudson.model.Result;
 import net.praqma.clearcase.test.junit.ClearCaseRule;
 import net.praqma.jenkins.configrotator.ConfigRotatorProject;
@@ -37,7 +39,7 @@ public class JENKINS17830 {
     public static ConfigRotatorRule2 crrule = new ConfigRotatorRule2( JENKINS17830.class );
 
     @Test
-    public void test() throws IOException {
+    public void test() throws IOException, InterruptedException {
         ProjectBuilder builder = new ProjectBuilder( new ClearCaseUCM( ccenv.getPVob() ) ).setName( "config-spec" );
         ConfigRotatorProject project = builder.getProject();
         project.addTarget( new ClearCaseUCMTarget( "a-baseline-1@" + ccenv.getPVob() + ", INITIAL, false" ) ).
@@ -45,10 +47,15 @@ public class JENKINS17830 {
 
         AbstractBuild<?, ?> build = crrule.buildProject( project.getJenkinsProject(), false, null );
 
+        FilePath path = new FilePath( project.getJenkinsProject().getLastBuiltOn().getWorkspaceFor( (FreeStyleProject)project.getJenkinsProject() ), "view/" + ccenv.getUniqueName() );
+        listPath( path );
+
         SystemValidator<ClearCaseUCMTarget> val = new SystemValidator<ClearCaseUCMTarget>( build );
         val.checkExpectedResult( Result.SUCCESS ).
                 checkAction( true ).
                 checkTargets( new ClearCaseUCMTarget( "a-baseline-1@" + ccenv.getPVob() + ", INITIAL, false" ), new ClearCaseUCMTarget( "b-baseline-1@" + ccenv.getPVob() + ", INITIAL, false" ) ).
+                addElementToPathCheck( path, new SystemValidator.Element( "Server", true ) ).
+                addElementToPathCheck( path, new SystemValidator.Element( "Client", true ) ).
                 checkCompatability( true ).
                 validate();
 
@@ -56,11 +63,22 @@ public class JENKINS17830 {
 
         AbstractBuild<?, ?> build2 = crrule.buildProject( project.getJenkinsProject(), false, null );
 
+        listPath( path );
+
         SystemValidator<ClearCaseUCMTarget> val2 = new SystemValidator<ClearCaseUCMTarget>( build2 );
         val2.checkExpectedResult( Result.SUCCESS ).
                 checkAction( true ).
                 checkTargets( new ClearCaseUCMTarget( "a-baseline-1@" + ccenv.getPVob() + ", INITIAL, false" ) ).
+                addElementToPathCheck( path, new SystemValidator.Element( "Server", true ) ).
+                addElementToPathCheck( path, new SystemValidator.Element( "Client", false ) ).
                 checkCompatability( true ).
                 validate();
+    }
+
+    protected void listPath( FilePath path ) throws IOException, InterruptedException {
+        logger.info( "Listing " + path + "(" + path.exists() + ")" );
+        for( FilePath f : path.list() ) {
+            logger.info( " * " + f );
+        }
     }
 }
